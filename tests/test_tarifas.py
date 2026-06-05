5e95051099d83f9723e69e29f2a1d234d792fa87
Maria Victoria <mgiraldo3163@cue.edu.co>
2026-06-05 15:22:56 -0500

    test: agregar pruebas red para cobro por hora o fraccion

--- tests/test_tarifas.py
+++ tests/test_tarifas.py
@@ -6,3 +6,21 @@ def test_primeros_30_minutos_son_gratis():
     tarifa = calcular_tarifa(minutos=30, vip=False)
 
     assert tarifa == 0
+
+
+def test_minuto_31_cobra_una_hora():
+    tarifa = calcular_tarifa(minutos=31, vip=False)
+
+    assert tarifa == 500
+
+
+def test_90_minutos_cobra_una_hora_o_fraccion():
+    tarifa = calcular_tarifa(minutos=90, vip=False)
+
+    assert tarifa == 500
+
+
+def test_91_minutos_cobra_dos_horas_o_fraccion():
+    tarifa = calcular_tarifa(minutos=91, vip=False)
+
+    assert tarifa == 1000
