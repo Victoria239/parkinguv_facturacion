c49df58ef8cc5c790931a08343cfd01684e89d36
Maria Victoria <mgiraldo3163@cue.edu.co>
2026-06-05 15:21:17 -0500

    test: agregar prueba red para primeros 30 minutos gratis

---
+++ tests/test_tarifas.py
@@ -0,0 +1,8 @@
+﻿import pytest
+from parkinguv.tarifas import calcular_tarifa
+
+
+def test_primeros_30_minutos_son_gratis():
+    tarifa = calcular_tarifa(minutos=30, vip=False)
+
+    assert tarifa == 0
